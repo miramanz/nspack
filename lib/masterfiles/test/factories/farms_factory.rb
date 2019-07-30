c8b31253302453f81797291488fdb66e4082f6f2
miramanz <miraclemanzongo@gmail.com>
2019-07-30 15:17:51 +0200

    new test

--- lib/masterfiles/test/factories/farms_factory.rb
+++ lib/masterfiles/test/factories/farms_factory.rb
@@ -9,104 +9,75 @@
 
 module MasterfilesApp
   module FarmsFactory
-    def create_farm_group(opts = {})
-      party_role_id = create_party_role
-
-      default = {
-        owner_party_role_id: party_role_id,
-        farm_group_code: Faker::Lorem.unique.word,
-        description: Faker::Lorem.word,
-        active: true,
-        created_at: '2010-01-01 12:00',
-        updated_at: '2010-01-01 12:00'
-      }
-      DB[:farm_groups].insert(default.merge(opts))
-    end
-
-    def create_party_role(opts = {})
-      party_id = create_party
-      role_id = create_role
-      organization_id = create_organization
-      person_id = create_person
-
-      default = {
-        party_id: party_id,
-        role_id: role_id,
-        organization_id: organization_id,
-        person_id: person_id,
-        active: true,
-        created_at: '2010-01-01 12:00',
-        updated_at: '2010-01-01 12:00'
-      }
-      DB[:party_roles].insert(default.merge(opts))
-    end
 
     def create_party(opts = {})
       default = {
-        party_type: Faker::Lorem.word,
-        active: true,
-        created_at: '2010-01-01 12:00',
-        updated_at: '2010-01-01 12:00'
+          party_type: 'O', # || 'P'
+          active: true
       }
       DB[:parties].insert(default.merge(opts))
     end
 
-    def create_role(opts = {})
+    def create_person(opts = {})
+      party_id = create_party(party_type: 'P')
       default = {
-        name: Faker::Lorem.word,
-        active: true,
-        created_at: '2010-01-01 12:00',
-        updated_at: '2010-01-01 12:00'
+          party_id: party_id,
+          title: Faker::Company.name.to_s,
+          first_name: Faker::Company.name.to_s,
+          surname: Faker::Company.name.to_s,
+          vat_number: Faker::Number.number(10),
+          active: true
       }
-      DB[:roles].insert(default.merge(opts))
+      {
+          id: DB[:people].insert(default.merge(opts)),
+          party_id: party_id
+      }
     end
 
     def create_organization(opts = {})
+      party_id = create_party(party_type: 'O')
       default = {
-        party_id: Faker::Number.number,
-        parent_id: Faker::Number.number,
-        short_description: Faker::Lorem.word,
-        medium_description: Faker::Lorem.word,
-        long_description: Faker::Lorem.word,
-        vat_number: Faker::Lorem.word,
-        variants: ['A', 'B', 'C'],
-        active: true,
-        created_at: '2010-01-01 12:00',
-        updated_at: '2010-01-01 12:00'
+          party_id: party_id,
+          parent_id: nil,
+          short_description: Faker::Company.unique.name.to_s,
+          medium_description: Faker::Company.name.to_s,
+          long_description: Faker::Company.name.to_s,
+          vat_number: Faker::Number.number(10),
+          active: true
       }
-      DB[:organizations].insert(default.merge(opts))
+      {
+          id: DB[:organizations].insert(default.merge(opts))
+      }
     end
 
-    def create_person(opts = {})
+    def create_role(opts = {})
+      existing_id = @fixed_table_set[:roles][:"#{opts[:name].downcase}"] if opts[:name]
+      return existing_id unless existing_id.nil?
+
       default = {
-        party_id: Faker::Number.number,
-        surname: Faker::Lorem.word,
-        first_name: Faker::Lorem.word,
-        title: Faker::Lorem.word,
-        vat_number: Faker::Lorem.word,
-        active: true,
-        created_at: '2010-01-01 12:00',
-        updated_at: '2010-01-01 12:00'
+          name: Faker::Lorem.unique.word,
+          active: true
       }
-      DB[:people].insert(default.merge(opts))
+      {
+          id: DB[:roles].insert(default.merge(opts))
+      }
     end
 
-    def create_farm(opts = {})
-      party_role_id = create_party_role
-      production_region_id = create_production_region
-      farm_group_id = create_farm_group
-
+    def create_party_role(party_type = 'O', role = nil, opts = {})
+      party_id = create_party(party_type: party_type)
+      role_id = opts[:role_id] || role ? create_role(name: role)[:id] : create_role[:id]
       default = {
-          owner_party_role_id: party_role_id,
-          pdn_region_id: production_region_id,
-          farm_group_id: farm_group_id,
-          farm_code: Faker::Lorem.unique.word,
-          description: Faker::Lorem.word,
-          active: true,
-          created_at: '2010-01-01 12:00',
-          updated_at: '2010-01-01 12:00'
+          party_id: party_id,
+          role_id: role_id,
+          active: true
       }
-      DB[:farms].insert(default.merge(opts))
+      default[:organization_id] = create_organization(party_id: party_id)[:id] if party_type == 'O'
+      default[:person_id] = create_person(party_id: party_id)[:id] if party_type == 'P'
+      final_options = default.merge(opts)
+     id = DB[:party_roles].insert(final_options)
+      {
+          id: id
+      }
     end
 
     def create_production_region(opts = {})
@@ -118,6 +89,39 @@ module MasterfilesApp
           updated_at: '2010-01-01 12:00'
       }
       DB[:production_regions].insert(default.merge(opts))
+    end
+
+    def create_farm_group(opts = {})
+      party_role_id = create_party_role(party_type = 'O')[:id]
+
+      default = {
+          owner_party_role_id: party_role_id,
+          farm_group_code: Faker::Lorem.unique.word,
+          description: Faker::Lorem.word,
+          active: true,
+          created_at: '2010-01-01 12:00',
+          updated_at: '2010-01-01 12:00'
+      }
+      DB[:farm_groups].insert(default.merge(opts))
+    end
+
+    def create_farm(opts = {})
+      party_role_id = create_party_role(party_type = 'O')
+      production_region_id = create_production_region
+      farm_group_id = create_farm_group
+
+      default = {
+          owner_party_role_id: party_role_id,
+          pdn_region_id: production_region_id,
+          farm_group_id: farm_group_id,
+          farm_code: Faker::Lorem.unique.word,
+          description: Faker::Lorem.word,
+          active: true,
+          puc_id: 1,
+          created_at: '2010-01-01 12:00',
+          updated_at: '2010-01-01 12:00'
+      }
+      DB[:farms].insert(default.merge(opts).delete(:puc_id))
     end
 
     def create_puc(opts = {})
@@ -139,13 +143,21 @@ module MasterfilesApp
           farm_id: farm_id,
           orchard_code: Faker::Lorem.unique.word,
           description: Faker::Lorem.word,
-          cultivar_ids: [1, 2, 3],
+          cultivar_ids: '{1, 2, 3}',
           active: true,
           created_at: '2010-01-01 12:00',
           updated_at: '2010-01-01 12:00',
           puc_id: puc_id
       }
       DB[:orchards].insert(default.merge(opts))
+    end
+
+    def create_farms_pucs(opts = {})
+      default = {
+          farm_id: create_farm,
+          puc_id: create_puc
+      }
+      DB[:farms_pucs].insert(default.merge(opts))
     end
 
   end
--- lib/masterfiles/test/interactors/test_orchard_interactor.rb
+++ lib/masterfiles/test/interactors/test_orchard_interactor.rb
@@ -3,14 +3,90 @@
 require File.join(File.expand_path('../../../../test', __dir__), 'test_helper')
 
 module MasterfilesApp
-  class TestOrchardInteractor < Minitest::Test
+  class TestOrchardInteractor < MiniTestWithHooks
+    include FarmsFactory
+
     def test_repo
-      repo = interactor.repo
-      # repo = interactor.send(:repo)
+      repo = interactor.send(:repo)
       assert repo.is_a?(MasterfilesApp::FarmRepo)
     end
 
+    def test_orchard
+      MasterfilesApp::FarmRepo.any_instance.stubs(:find_orchard).returns(fake_orchard)
+      entity = interactor.send(:orchard, 1)
+      assert entity.is_a?(Orchard)
+    end
+
+    def test_create_orchard
+      attrs = fake_orchard.to_h.reject { |k, _| k == :id }
+      res = interactor.create_orchard(attrs)
+      assert res.success, "#{res.message} : #{res.errors.inspect}"
+      assert_instance_of(Orchard, res.instance)
+      assert res.instance.id.nonzero?
+    end
+
+    def test_create_orchard_fail
+      attrs = fake_orchard(orchard_code: nil).to_h.reject { |k, _| k == :id }
+      res = interactor.create_orchard(attrs)
+      refute res.success, 'should fail validation'
+      assert_equal ['must be filled'], res.errors[:orchard_code]
+    end
+
+    def test_update_orchard
+      id = create_orchard
+      attrs = interactor.send(:repo).find_hash(:orchards, id).reject { |k, _| k == :id }
+      value = attrs[:orchard_code]
+      attrs[:orchard_code] = 'a_change'
+      res = interactor.update_orchard(id, attrs)
+      assert res.success, "#{res.message} : #{res.errors.inspect}"
+      assert_instance_of(Orchard, res.instance)
+      assert_equal 'a_change', res.instance.orchard_code
+      refute_equal value, res.instance.orchard_code
+    end
+
+    def test_update_orchard_fail
+      id = create_orchard
+      attrs = interactor.send(:repo).find_hash(:orchards, id).reject { |k, _| %i[id orchard_code].include?(k) }
+      value = attrs[:description]
+      attrs[:description] = 'a_change'
+      res = interactor.update_orchard(id, attrs)
+      refute res.success, "#{res.message} : #{res.errors.inspect}"
+      assert_equal ['is missing'], res.errors[:orchard_code]
+      after = interactor.send(:repo).find_hash(:orchards, id)
+      refute_equal 'a_change', after[:description]
+      assert_equal value, after[:description]
+    end
+
+    def test_delete_orchard
+      id = create_orchard
+      assert_count_changed(:orchards, -1) do
+        res = interactor.delete_orchard(id)
+        assert res.success, res.message
+      end
+    end
+
     private
+
+    def orchard_attrs
+      farm_id = create_farm
+      puc_id = create_puc
+
+      {
+          id: 1,
+          farm_id: farm_id,
+          puc_id: puc_id,
+          orchard_code: Faker::Lorem.unique.word,
+          description: 'ABC',
+          cultivar_ids: [1, 2, 3],
+          puc_code: 'ABC',
+          cultivar_names: 'ABC',
+          active: true
+      }
+    end
+
+    def fake_orchard(overrides = {})
+      Orchard.new(orchard_attrs.merge(overrides))
+    end
 
     def interactor
       @interactor ||= OrchardInteractor.new(current_user, {}, {}, {})
