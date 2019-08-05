44df13745838219a882a2b5b7402d5f88f74af6e
miramanz <miraclemanzongo@gmail.com>
2019-08-05 11:54:20 +0200

    cultivar and calendar tests

---
+++ lib/masterfiles/test/factories/calendar_factory.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# ========================================================= #
+# NB. Scaffolds for test factories should be combined       #
+#     - Otherwise you'll have methods for the same table in #
+#       several factories.                                  #
+#     - Rather create a factory for several related tables  #
+# ========================================================= #
+
+module MasterfilesApp
+  module CalendarFactory
+    def create_season_group(opts = {})
+      default = {
+        season_group_code: Faker::Lorem.unique.word,
+        description: Faker::Lorem.word,
+        season_group_year: Time.now.year,
+        active: true
+      }
+      DB[:season_groups].insert(default.merge(opts))
+    end
+
+    def create_season(opts = {})
+      season_group_id = create_season_group
+      commodity_id = create_commodity
+
+      default = {
+        season_group_id: season_group_id,
+        commodity_id: commodity_id,
+        season_code: Faker::Lorem.unique.word,
+        description: Faker::Lorem.word,
+        season_year: Time.now.year,
+        active: true
+      }
+      DB[:seasons].insert(default.merge(opts))
+    end
+  end
+end
---
+++ lib/masterfiles/test/factories/cultivar_factory.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# ========================================================= #
+# NB. Scaffolds for test factories should be combined       #
+#     - Otherwise you'll have methods for the same table in #
+#       several factories.                                  #
+#     - Rather create a factory for several related tables  #
+# ========================================================= #
+
+module MasterfilesApp
+  module CultivarFactory
+    def create_cultivar_group(opts = {})
+      default = {
+        cultivar_group_code: Faker::Lorem.unique.word,
+        description: Faker::Lorem.word
+      }
+      DB[:cultivar_groups].insert(default.merge(opts))
+    end
+
+    def create_cultivar(opts = {})
+      commodity_id = create_commodity
+      cultivar_group_id = create_cultivar_group
+
+      default = {
+        commodity_id: commodity_id,
+        cultivar_group_id: cultivar_group_id,
+        cultivar_name: Faker::Lorem.unique.word,
+        description: Faker::Lorem.word
+      }
+      DB[:cultivars].insert(default.merge(opts))
+    end
+  end
+end
