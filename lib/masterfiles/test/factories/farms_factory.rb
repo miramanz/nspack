ab302c31283a4b0fb84f14a78f87d727340bedc1
James Silberbauer <jamessil@telkomsa.net>
2019-08-07 14:23:02 +0200

    Fix Orchard interactor tests

--- lib/masterfiles/test/factories/farms_factory.rb
+++ lib/masterfiles/test/factories/farms_factory.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
-# ========================================================= #
-# NB. Scaffolds for test factories should be combined       #
-#     - Otherwise you'll have methods for the same table in #
-#       several factories.                                  #
-#     - Rather create a factory for several related tables  #
-# ========================================================= #
-
 module MasterfilesApp
   module FarmsFactory
     def create_production_region(opts = {})
@@ -64,12 +57,13 @@ module MasterfilesApp
     def create_orchard(opts = {})
       farm_id = create_farm[:id]
       puc_id = create_puc
+      cultivar_id = create_cultivar
 
       default = {
         farm_id: farm_id,
         orchard_code: Faker::Lorem.unique.word,
         description: Faker::Lorem.word,
-        cultivar_ids: '{1}',
+        cultivar_ids: "{#{cultivar_id}}",
         active: true,
         puc_id: puc_id
       }
