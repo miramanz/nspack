c353b45e3424f5858c2658eda6ea5f1322b7ffae
James Silberbauer <jamessil@telkomsa.net>
2019-08-08 18:37:49 +0200

    Remove test for deleted resource type routes

--- test/routes/production/resources/test_resource_type_routes.rb
+++ test/routes/production/resources/test_resource_type_routes.rb
@@ -6,22 +6,6 @@ class TestPlantResourceTypeRoutes < RouteTester
 
   INTERACTOR = ProductionApp::ResourceTypeInteractor
 
-  def test_edit
-    authorise_pass!
-    ensure_exists!(INTERACTOR)
-    Production::Resources::PlantResourceType::Edit.stub(:call, bland_page) do
-      get 'production/resources/plant_resource_types/1/edit', {}, 'rack.session' => { user_id: 1 }
-    end
-    expect_bland_page
-  end
-
-  def test_edit_fail
-    authorise_fail!
-    ensure_exists!(INTERACTOR)
-    get 'production/resources/plant_resource_types/1/edit', {}, 'rack.session' => { user_id: 1 }
-    expect_permission_error
-  end
-
   def test_show
     authorise_pass!
     ensure_exists!(INTERACTOR)
@@ -38,25 +22,6 @@ class TestPlantResourceTypeRoutes < RouteTester
     expect_permission_error
   end
 
-  def test_update
-    authorise_pass!
-    ensure_exists!(INTERACTOR)
-    row_vals = Hash.new(1)
-    INTERACTOR.any_instance.stubs(:update_plant_resource_type).returns(ok_response(instance: row_vals))
-    patch_as_fetch 'production/resources/plant_resource_types/1', {}, 'rack.session' => { user_id: 1, last_grid_url: DEFAULT_LAST_GRID_URL }
-    expect_json_update_grid
-  end
-
-  def test_update_fail
-    authorise_pass!
-    ensure_exists!(INTERACTOR)
-    INTERACTOR.any_instance.stubs(:update_plant_resource_type).returns(bad_response)
-    Production::Resources::PlantResourceType::Edit.stub(:call, bland_page) do
-      patch_as_fetch 'production/resources/plant_resource_types/1', {}, 'rack.session' => { user_id: 1, last_grid_url: DEFAULT_LAST_GRID_URL }
-    end
-    expect_json_replace_dialog(has_error: true)
-  end
-
   def test_delete
     authorise_pass!
     ensure_exists!(INTERACTOR)
@@ -71,40 +36,5 @@ class TestPlantResourceTypeRoutes < RouteTester
     INTERACTOR.any_instance.stubs(:delete_plant_resource_type).returns(bad_response)
     delete_as_fetch 'production/resources/plant_resource_types/1', {}, 'rack.session' => { user_id: 1, last_grid_url: DEFAULT_LAST_GRID_URL }
     expect_json_error
-  end
-
-  def test_new
-    authorise_pass!
-    ensure_exists!(INTERACTOR)
-    Production::Resources::PlantResourceType::New.stub(:call, bland_page) do
-      get  'production/resources/plant_resource_types/new', {}, 'rack.session' => { user_id: 1 }
-    end
-    expect_bland_page
-  end
-
-  def test_new_fail
-    authorise_fail!
-    ensure_exists!(INTERACTOR)
-    get 'production/resources/plant_resource_types/new', {}, 'rack.session' => { user_id: 1 }
-    expect_permission_error
-  end
-
-  def test_create_remotely
-    authorise_pass!
-    ensure_exists!(INTERACTOR)
-    row_vals = Hash.new(1)
-    INTERACTOR.any_instance.stubs(:create_plant_resource_type).returns(ok_response(instance: row_vals))
-    post_as_fetch 'production/resources/plant_resource_types', {}, 'rack.session' => { user_id: 1, last_grid_url: DEFAULT_LAST_GRID_URL }
-    expect_json_add_to_grid(has_notice: true)
-  end
-
-  def test_create_remotely_fail
-    authorise_pass!
-    ensure_exists!(INTERACTOR)
-    INTERACTOR.any_instance.stubs(:create_plant_resource_type).returns(bad_response)
-    Production::Resources::PlantResourceType::New.stub(:call, bland_page) do
-      post_as_fetch 'production/resources/plant_resource_types', {}, 'rack.session' => { user_id: 1, last_grid_url: DEFAULT_LAST_GRID_URL }
-    end
-    expect_json_replace_dialog
   end
 end
